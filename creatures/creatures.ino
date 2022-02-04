51d50d863968934ebdf8e2e6507ce99135388f43
Nick Donaldson <ndonald2@gmail.com>
2022-02-03 18:37:00 -0700

    Adjustments adjustments

--- creatures/creatures.ino
+++ creatures/creatures.ino
@@ -1,10 +1,10 @@
 #include <Adafruit_MotorShield.h>
 
-static const float LIGHT_THRESH = 0.7;
-static const float RPM_SCALE = 100.0;
+static const float LIGHT_THRESH = 0.6;
+static const float RPM_SCALE = 150.0;
 
-static const float PRESS_MIN = 600.0;
-static const float PRESS_MAX = 900.0;
+static const float PRESS_MIN = 650.0;
+static const float PRESS_MAX = 850.0;
 
 class Smooth {
 
@@ -38,11 +38,20 @@ class Creature {
 
 public:
 
-  Creature(int lightPin, int pressPin, Adafruit_DCMotor *motor) :
-    m_lightPin(lightPin),
-    m_pressPin(pressPin),
-    m_motor(motor)
+  Creature(
+    int lightPin, 
+    int pressPin, 
+    int vibePin, 
+    int ledPin,
+    Adafruit_DCMotor *motor
+  ) : m_lightPin(lightPin),
+      m_pressPin(pressPin),
+      m_vibePin(vibePin),
+      m_ledPin(ledPin),
+      m_motor(motor)
   {
+    pinMode(vibePin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
     m_motor->setSpeed(0);
     m_motor->run(FORWARD);
   }
@@ -56,21 +65,25 @@ private:
 
   int m_lightPin;
   int m_pressPin;
+  int m_vibePin;
+  int m_ledPin;
   Adafruit_DCMotor *m_motor;
-  Smooth smooth = Smooth(8.0, 20.0);
+  Smooth smooth = Smooth(5.0, 60.0);
 
   void updateMotorSpeed() {
     float lightAmt = analogRead(m_lightPin) / 1023.0f;
     lightAmt = smooth.Process(lightAmt);
     float motorSpeed = ((lightAmt - LIGHT_THRESH) / (1.0 - LIGHT_THRESH)) * RPM_SCALE;
     m_motor->setSpeed(fmax(0.0, motorSpeed));
+    digitalWrite(m_ledPin, lightAmt > 0.2 ? HIGH : LOW);
   }
 
   void updatePressure() {
     float raw = analogRead(m_pressPin);
     float norm = (raw - PRESS_MIN) / (PRESS_MAX - PRESS_MIN);
     norm = fmax(0.0, fmin(1.0, norm));
-    Serial.println(norm);
+    analogWrite(m_vibePin, norm * 255);
+//    Serial.println(norm);
   }
 };
 
@@ -83,8 +96,7 @@ void setup() {
       Serial.println("Could not find Motor Shield. Check wiring.");
     while (1);
   }
-
-  creature1 = new Creature(A0, A1, motorShield.getMotor(3));
+  creature1 = new Creature(A0, A1, A2, D30, motorShield.getMotor(3));
 }
 
 void loop() {
